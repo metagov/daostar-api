387f94ad91a7e51393f7c116229045eb5d85c56f
Luke Miller <millerluke1364@gmail.com>
2022-07-07 12:56:46 -0400

    removed static file hosting

--- app/__init__.py
+++ app/__init__.py
@@ -7,27 +7,7 @@ from app.resources.resolve import ResolveSchema
 
 flask_app = Flask('DAOstar API')
 api = Api(flask_app)
-cors = CORS(flask_app, origins=["http://127.0.0.1:5500", "https://daostar.org"])
-
-@flask_app.get('/')
-async def query():
-    return open('static/query.html', 'r').read()
-
-@flask_app.get('/generate')
-async def display_generator():
-    return open('static/generator.html', 'r').read()
-
-@flask_app.get('/dao')
-async def contract():
-    return open('static/contract.html', 'r').read()
-
-@flask_app.get('/css/style.css')
-async def get_css():
-    return Response(
-        response=open('static/style.css', 'r').read(),
-        status=200,
-        mimetype="text/css")
-     
+cors = CORS(flask_app, origins=["http://127.0.0.1:5000", "https://daostar.org"])
 
 api.add_resource(MutableSchema, '/mutable', '/mutable/', '/mutable/<caip>')
 # api.add_resource(Members, '/mutable/<caip>/members')
