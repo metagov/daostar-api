719727853e92791d4a88ea9e4f7a0e72863b6167
lukvmil <millerluke1364@gmail.com>
2022-07-12 19:03:44 +0000

    updated immutable endpoint to use same format as mutable endpoint

--- app/resources/immutable.py
+++ app/resources/immutable.py
@@ -1,14 +1,19 @@
 from flask import request
 from flask_restful import Resource, abort
 from app.connectors import ipfs, pinata
-from app.utils.schema import validate_schema, format_schema
+from app.utils.schema import validate_json, validate_schema, format_schema
 from app.connectors.aws import immutable, get_item, put_item
 from app.constants import Web
 
 class CreateImmutableSchema(Resource):
     def post(self):
-        data = validate_schema()
+        body = validate_json()
+
+        if 'data' not in body: abort(400, message=f'missing required field: data')
         
+        schema = body['data']
+        data = validate_schema(schema)
+
         cid1 = ipfs.add_json(format_schema(data))
         cid2 = pinata.add_json(format_schema(data))
         immutable(put_item, cid1, data)
