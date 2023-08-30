c59956049719783a7a54c7672c1a69ba12a08a58
Rashmi-278 <rashmiabbigeri@gmail.com>
2023-08-30 18:56:11 +0530

    update DAO URI fields

--- app/validators/dao.py
+++ app/validators/dao.py
@@ -10,6 +10,8 @@ class DaoUriSchema(BaseSchema):
     issuersURI     = fields.String(default=None)
     activityLogURI = fields.String(default=None)
     governanceURI  = fields.String(default=None)
+    contractRegistryURI = fields.String(default=None)
+    managerAddress  = fields.String(default=None)
 
 class InputCaipWithDaoSchema(Schema):
     data = fields.Nested(DaoUriSchema, required=True)
