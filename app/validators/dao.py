8c18f8cae33a4ef8adbd0260b651b26b3c490ce6
Rashmi-278 <rashmiabbigeri@gmail.com>
2024-04-11 20:37:10 +0530

    omit blank fields in DAO URI

--- app/validators/dao.py
+++ app/validators/dao.py
@@ -2,22 +2,16 @@ from marshmallow import Schema, fields, post_load
 from app.validators.fields import Caip10, Uri
 from app.validators.base import BaseSchema
 
-# class BaseSchema(Schema):
-#     @post_load
-#     def remove_empty_fields(self, data, **kwargs):
-#         """Remove keys with None values or empty strings."""
-#         return {key: value for key, value in data.items() if value not in [None, "", 'null']}
-
 class DaoUriSchema(BaseSchema):
-    name           = fields.String(default=None)
-    description    = fields.String(default=None)
-    membersURI     = fields.String(default=None)
-    proposalsURI   = fields.String(default=None)
-    issuersURI     = fields.String(default=None)
-    activityLogURI = fields.String(default=None)
-    governanceURI  = fields.String(default=None)
-    contractsRegistryURI = fields.String(default=None)
-    managerAddress  = fields.String(default=None)
+    name = fields.String(skip_if=lambda x: x in [None, ""], required=False)
+    description = fields.String(skip_if=lambda x: x in [None, ""], required=False)
+    membersURI = fields.String(skip_if=lambda x: x in [None, ""], required=False)
+    proposalsURI = fields.String(skip_if=lambda x: x in [None, ""], required=False)
+    issuersURI = fields.String(skip_if=lambda x: x in [None, ""], required=False)
+    activityLogURI = fields.String(skip_if=lambda x: x in [None, ""], required=False)
+    governanceURI = fields.String(skip_if=lambda x: x in [None, ""], required=False)
+    contractsRegistryURI = fields.String(skip_if=lambda x: x in [None, ""], required=False)
+    managerAddress = fields.String(skip_if=lambda x: x in [None, ""], required=False)
 
 class InputCaipWithDaoSchema(Schema):
     data = fields.Nested(DaoUriSchema, required=True)
