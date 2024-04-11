907c535243e0290d31eaef04c76f0b5ee4d0a3e7
Rashmi-278 <rashmiabbigeri@gmail.com>
2024-04-11 20:42:05 +0530

    omit blank fields in DAO URI try1

--- app/validators/dao.py
+++ app/validators/dao.py
@@ -1,4 +1,4 @@
-from marshmallow import Schema, fields, post_load
+from marshmallow import Schema, fields, pre_dump
 from app.validators.fields import Caip10, Uri
 from app.validators.base import BaseSchema
 
@@ -12,6 +12,15 @@ class DaoUriSchema(BaseSchema):
     governanceURI = fields.String(skip_if=lambda x: x in [None, ""], required=False)
     contractsRegistryURI = fields.String(skip_if=lambda x: x in [None, ""], required=False)
     managerAddress = fields.String(skip_if=lambda x: x in [None, ""], required=False)
+
+    @pre_dump
+    def remove_empty_fields(self, data, **kwargs):
+        """Remove fields with None or empty string values before dumping."""
+        data_dict = data.__dict__.copy()
+        for key in list(data_dict.keys()):  # list() to avoid RuntimeError due to change in dict size
+            if data_dict[key] in [None, ""]:
+                del data_dict[key]
+        return data_dict
 
 class InputCaipWithDaoSchema(Schema):
     data = fields.Nested(DaoUriSchema, required=True)
